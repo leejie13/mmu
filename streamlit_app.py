a77c73ffd8e3c16b3da6c354549b451323e76afc
leejie13 <1211101529@student.mmu.edu.my>
2025-11-17 10:36:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ for fact in env.facts():
   if fact.template.name == 'result':
     results.append (fact['name']) #why assert the fact?
 
-st.write(results[0], "output")
+st.write(results[0],"output")
